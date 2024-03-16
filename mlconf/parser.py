5eb6364fd1700fdd2477fc1e497120c8a126cd4e
sashank-tirumala <stsashank6@gmail.com>
2024-03-15 22:38:27 -0700

    added basic input stream object

--- mlconf/parser.py
+++ mlconf/parser.py
@@ -6,5 +6,48 @@ class Parser:
         return None
 
 
+class InputStream:
+    def __init__(self, input):
+        self.input = input
+        self.pos = 0
+        self.line = 1
+        self.col = 0
+
+    def next(self):
+        ch = self.input[self.pos]
+        self.pos += 1
+        if ch == "\n":
+            self.line += 1
+            self.col = 0
+        else:
+            self.col += 1
+        return ch
+
+    def peek(self):
+        self.pos
+        return self.input[self.pos]
+
+    def eof(self):
+        self.pos
+        return self.pos >= len(self.input)
+
+    def croak(self, msg):
+        self.line
+        self.col
+        raise Exception(f"{msg} ({self.line}:{self.col})")
+
+
 def parse(config: str):
     return Parser(config).parse()
+
+
+if __name__ == "__main__":
+    config = """
+    string: test
+    string2: "test"
+    string3: 'test:123'
+    string4: "test"
+    """
+    inp = InputStream(config)
+    while not inp.eof():
+        print(inp.next(), end="")
