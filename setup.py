116560bcb151d8aa48b15155b95d2da266a9ec78
sashank-tirumala <stsashank6@gmail.com>
2024-03-21 00:30:34 -0700

    improved setup.py tools

--- setup.py
+++ setup.py
@@ -1,3 +1,15 @@
-import setuptools
+from setuptools import find_packages, setup
 
-setuptools.setup()
+setup(
+    name="mlconf",
+    version="0.1.0",
+    packages=find_packages(),
+    python_requires=">=3.6",
+    install_requires=[],
+    extras_require={
+        "dev": [
+            "pytest",
+            "sphinx",
+        ],
+    },
+)
